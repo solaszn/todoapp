24b0bc0d15faa300b7d0d53d000a98c1dd7ba5a9
sola olawuyi <56759169+solaszn@users.noreply.github.com>
2023-06-10 11:44:12 +0100

    Initial commit

---
+++ pages/layout.tsx
@@ -0,0 +1,18 @@
+import "@/styles/globals.css";
+
+export const metadata = {
+    title: "Todo App",
+    description: "Simple Todo App built with NextJS 13",
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    )
+}
